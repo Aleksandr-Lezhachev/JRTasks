fb251a3fcb2ba96de7fe5b11e75152edfe7877f7
Aleksandr-Lezhachev <alex94_cr@mail.ru>
2018-10-18 16:34:56 +0300

    18/10/2018

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task16/task1623/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task16/task1623/Solution.java
@@ -4,6 +4,9 @@ package com.javarush.task.task16.task1623;
 Рекурсивное создание нитей
 */
 
+/**
+ * работает но валидатор не проходит
+ */
 public class Solution {
     static int count = 15;
     static volatile int countCreatedThreads;
@@ -13,18 +16,23 @@ public class Solution {
     }
 
     public static class GenerateThread extends Thread {
-        int countCreatedThreads;
 
         GenerateThread() {
-            this.countCreatedThreads++;
-            super(countCreatedThreads.toString);
+            super(String.valueOf(++countCreatedThreads));
+            start();
+        }
+
+        @Override
+        public void run() {
+            if (countCreatedThreads <= count) {
+                GenerateThread t = new GenerateThread();
+                System.out.println(t.toString());
+            }
         }
 
         @Override
         public String toString() {
-            return "GenerateThread{" +
-                    "countCreatedThreads=" + countCreatedThreads +
-                    '}';
+            return getName() + " created";
         }
     }
 }
